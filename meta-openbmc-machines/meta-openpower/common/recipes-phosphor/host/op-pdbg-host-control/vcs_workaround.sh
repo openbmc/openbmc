4afd0ddcf4722de4ef4a7d6ba7cbe5de7f6012d8
Sergey Solomin <sergey.solomin@us.ibm.com>
2016-11-28 12:17:09 -0600

    Put the CFAM/FSI slave into async mode prior to starting the SBE
    Async mode changes the CFAM clock to an internal reference clock
    instead of the FSI clock (which on SoftFSI is intermittent).
    
    Change-Id: I8af7989de72774aae0e6ffb91bce7e70bd6592a1
    Signed-off-by: Sergey Solomin <sergey.solomin@us.ibm.com>

--- meta-openbmc-machines/meta-openpower/common/recipes-phosphor/host/op-pdbg-host-control/vcs_workaround.sh
+++ meta-openbmc-machines/meta-openpower/common/recipes-phosphor/host/op-pdbg-host-control/vcs_workaround.sh
@@ -1,6 +1,11 @@
 #!/bin/sh
 
 gpioutil -p D0 -d out -v 1   # enable fsi link
+
+#put the CFAM/FSI slave into async mode
+pdbg-p9 -b fsi putcfam 0x900 0x1
+pdbg-p9 -b fsi putcfam 0x2801 0x80C00000
+
 putcfam pu 2810 15 1 0       # Unfence PLL controls
 putcfam pu 281A  1 1 f       # Assert Perv chiplet endpoint reset, just in case
 putcfam pu 281A 31 1 f       # Enable Nest PLL
