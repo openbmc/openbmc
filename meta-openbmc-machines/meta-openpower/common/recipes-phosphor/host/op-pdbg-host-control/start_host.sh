01605a1baf30cf1a10de8ea9fe09b81d97925f31
Sergey Solomin <sergey.solomin@us.ibm.com>
2016-12-12 14:17:44 -0600

    Add P9 putcfam support for vcs_workaround and start_host scripts
    Add P9 putcfam support for vcs_workaround and start_host scripts
    
    Partially resolves openbmc/openbmc#647
    
    Change-Id: Ia27865b63549e55bd1236ab3c43bb95f5693fe1d
    Signed-off-by: Sergey Solomin <sergey.solomin@us.ibm.com>

--- meta-openbmc-machines/meta-openpower/common/recipes-phosphor/host/op-pdbg-host-control/start_host.sh
+++ meta-openbmc-machines/meta-openpower/common/recipes-phosphor/host/op-pdbg-host-control/start_host.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 
 # send putcfam command to have SBE start IPL
-putcfam pu 2801 0 1 1 -ib
+pdbg-p9 -b fsi putcfam 0x2801 0x80000000 0x80000000
+
--- meta-openbmc-machines/meta-openpower/common/recipes-phosphor/host/op-pdbg-host-control/vcs_workaround.sh
+++ meta-openbmc-machines/meta-openpower/common/recipes-phosphor/host/op-pdbg-host-control/vcs_workaround.sh
@@ -1,6 +1,32 @@
 #!/bin/sh
 
-putcfam pu 0900 31 1 1 -ib   # put the CFAM/FSI slave into async mode
-putcfam pu 2810 15 1 0       # Unfence PLL controls
-putcfam pu 281A  1 1 f       # Assert Perv chiplet endpoint reset, just in case
-putcfam pu 281A 31 1 f       # Enable Nest PLL
+# #############################################################
+# usage: putcfam address andmask ormask
+putcfam()
+{
+din=`pdbg-p9 -b fsi getcfam $1` &&
+{
+  din=`echo $din | cut -d ' ' -f 2`
+  dout=`printf "0x%016x" $(($(($din & $2)) | $3))`
+  pdbg-p9 -b fsi putcfam $1 $dout
+}
+return
+}
+
+# #############################################################
+# P9 dd1 required workaround needed before powering VCS rails
+p9_dd1_vcs_workaround()
+{
+putcfam 0x2810 0xFFFEFFFF 0 &&            # Unfence PLL controls
+putcfam 0x281A 0xFFFFFFFF 0x80000000 &&   # Assert Perv chiplet endpoint reset, just in case
+putcfam 0x281A 0xFFFFFFFF 1               # Enable Nest PLL
+return
+}
+
+# put the CFAM/FSI slave into async mode
+pdbg-p9 -b fsi putcfam 0x900 0x1
+pdbg-p9 -b fsi putcfam 0x2801 0x80C00000
+
+# run workaround
+p9_dd1_avsbus_workaround
+
