4e7e912ef9fc1095bcca4ea04fff35a751f75f32
Sergey Solomin <sergey.solomin@us.ibm.com>
2016-12-12 14:17:44 -0600

    Add P9 putcfam support for vcs_workaround and start_host scripts
    Add P9 putcfam support for vcs_workaround and start_host scripts
    
    Partially resolves openbmc/openbmc#647
    
    Change-Id: Ia27865b63549e55bd1236ab3c43bb95f5693fe1d
    Signed-off-by: Sergey Solomin <sergey.solomin@us.ibm.com>

--- meta-openbmc-machines/meta-openpower/common/recipes-phosphor/host/op-pdbg-host-control/vcs_workaround.sh
+++ meta-openbmc-machines/meta-openpower/common/recipes-phosphor/host/op-pdbg-host-control/vcs_workaround.sh
@@ -1,6 +1,14 @@
 #!/bin/sh
 
-putcfam pu 0900 31 1 1 -ib   # put the CFAM/FSI slave into async mode
-putcfam pu 2810 15 1 0       # Unfence PLL controls
-putcfam pu 281A  1 1 f       # Assert Perv chiplet endpoint reset, just in case
-putcfam pu 281A 31 1 f       # Enable Nest PLL
+# ################################################
+# P9 workaround before powering VCS rails
+
+# put the CFAM/FSI slave into async mode
+pdbg -b fsi putcfam 0x900 0x1
+# Unfence PLL controls
+pdbg -b fsi putcfam 0x2810 0xFFFEFFFF 0
+# Assert Perv chiplet endpoint reset, just in case
+pdbg -b fsi putcfam 0x281A 0xFFFFFFFF 0x40000000
+# Enable Nest PLL
+pdbg -b fsi putcfam 0x281A 0xFFFFFFFF 1
+
