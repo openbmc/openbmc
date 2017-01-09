14ede84fc10862a19ace64581d7ad1f00a4e031e
Matt Spinler <spinler@us.ibm.com>
2017-01-09 09:28:50 -0600

    Add multi-p9 support to the pdbg scripts
    start_host.sh and vcs_workaround.sh need changes
    to support multiple P9s.  Also bumped the pdbg
    version which is coreqed with the script changes.
    
    Change-Id: I5536b548315b5f9bf4fd4cb742c686a6ce12b6d0
    Signed-off-by: Matt Spinler <spinler@us.ibm.com>

--- meta-openbmc-machines/meta-openpower/common/recipes-phosphor/host/op-pdbg-host-control/vcs_workaround.sh
+++ meta-openbmc-machines/meta-openpower/common/recipes-phosphor/host/op-pdbg-host-control/vcs_workaround.sh
@@ -7,18 +7,28 @@ DEVICE_OPT=${1:+-d $1}
 
 putcfam()
 {
-    $PDBG -b fsi $DEVICE_OPT putcfam $1 $2 $3
+    $PDBG -b fsi $DEVICE_OPT $1 putcfam $2 $3 $4
 }
 
 # P9 dd1 required workaround needed before powering VCS rails
 p9_dd1_vcs_workaround()
 {
-    putcfam 0x2810 0x00000000 0x00010000 &&  # Unfence PLL controls
-    putcfam 0x281A 0x40000000 0x40000000 &&  # Assert Perv chiplet endpoint reset, just in case
-    putcfam 0x281A 0x00000001 0x00000001     # Enable Nest PLL
+    putcfam -a 0x2810 0x00000000 0x00010000 &&  # Unfence PLL controls
+    putcfam -a 0x281A 0x40000000 0x40000000 &&  # Assert Perv chiplet endpoint reset, just in case
+    putcfam -a 0x281A 0x00000001 0x00000001     # Enable Nest PLL
 }
 
 # Put the CFAM/FSI slave into async mode
-putcfam 0x900 1
+putcfam -p0 0x900 1
+
+#Set hMFSI error recovery
+putcfam -p0 0x34b8 0x0000c000
+
+#Set hMFSI timings
+putcfam -p0 0x3400 0xd0040110
+putcfam -p0 0x3401 0xffff0000
+
+#Enable hMFST ports
+putcfam -p0 0x3404 0xf0000000
 
 p9_dd1_vcs_workaround
