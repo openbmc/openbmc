0c4f635c395ec316b6895247f223e4fec12ab05b
Patrick Venture <venture@google.com>
2017-04-10 15:21:32 -0700

    Provide the quanta host power control.
      systemctl start obmc-chassis-stop@0.target
      systemctl start obmc-chassis-start@0.target
    
    Added the host-gpio.service on boot to enable the power on
    and power off scripts.
    
    Testing: The scripts were launched with the state change commands above,
    however they were modified such that they didn't rely on the old
    namespace.
    
    Change-Id: I4a205e60c8f43f626f3ea0d4fedf182ccf4700ba
    Signed-off-by: Patrick Venture <venture@google.com>

---
+++ meta-openbmc-machines/meta-x86/meta-quanta/meta-q71l/recipes-phosphor/powerctrl/files/init_once.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+# Set all output GPIOs as such and drive them with reasonable values.
+function set_gpio_active_low() {
+  if [ $# -ne 2 ]; then
+    echo "set_gpio_active_low: need both GPIO# and initial level";
+    return;
+  fi
+
+  echo $1 > /sys/class/gpio/export
+  echo 1 > /sys/class/gpio/gpio$1/active_low
+  echo $2 > /sys/class/gpio/gpio$1/direction
+}
+
+GPIO_BASE=$(cat /sys/class/gpio/gpiochip*/base)
+
+# FM_BMC_READY_N, GPIO Q4, active low
+set_gpio_active_low $((${GPIO_BASE} + 128 + 4)) high
+
+# FM_BMC_SSB_SMI_LPC_N, GPIO Q6, active low
+set_gpio_active_low $((${GPIO_BASE} + 128 + 6)) high
+
+# FM_BMC_SYS_THROTTLE_N, GPIO A3, active low
+set_gpio_active_low $((${GPIO_BASE} + 0 + 3)) high
+
+# FM_BMC_SSB_SCI_LPC_N, GPIO E4, active low
+set_gpio_active_low $((${GPIO_BASE} + 32 + 4)) high
+
+# FP_PWR_BTN_PASS_R_N, GPIO D3, active low
+set_gpio_active_low $((${GPIO_BASE} + 24 + 3)) high
+
+exit 0;
