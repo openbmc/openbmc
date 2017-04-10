906eb250ee6f257f2eb765145c7d3f70b34e6828
Patrick Venture <venture@google.com>
2017-04-10 15:21:32 -0700

    Provide the quanta host power control.
      systemctl start obmc-host-stop@0.target
      systemctl start obmc-host-start@0.target
    
    Added the host-gpio.service on boot to enable the power on
    and power off scripts.
    
    Testing: The scripts were launched with the state change commands above,
    however they were modified such that they didn't rely on the old
    namespace.
    
    The GPIOs basically trigger the power button on the front panel.
    
    Change-Id: I4a205e60c8f43f626f3ea0d4fedf182ccf4700ba
    Signed-off-by: Patrick Venture <venture@google.com>

---
+++ meta-openbmc-machines/meta-x86/meta-quanta/meta-q71l/recipes-phosphor/quanta-powerctrl/files/poweroff.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+GPIO_BASE=$(cat /sys/class/gpio/gpiochip*/base)
+GPIO_NUM=$(($GPIO_BASE + 24 + 3))
+
+echo 1 > /sys/class/gpio/gpio${GPIO_NUM}/value
+sleep 5
+echo 0 > /sys/class/gpio/gpio${GPIO_NUM}/value
+
+exit 0;
