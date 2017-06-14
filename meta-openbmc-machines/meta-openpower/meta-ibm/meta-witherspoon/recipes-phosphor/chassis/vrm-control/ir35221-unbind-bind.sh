a659de30a06ae83babb421f7baceae08e2992674
Christopher Bostic <cbostic@linux.vnet.ibm.com>
2017-06-14 09:55:35 -0500

    Check for ir35221 link presence prior to bind/unbind
    Check for /sys/bus/i2c/driversir35221/4-0070, 4-0071, 5-0070,
    5-0071 on unbind.
    
    Change-Id: I3c64682bcceffc6313625be1afadd38e090bcc63
    Signed-off-by: Christopher Bostic <cbostic@linux.vnet.ibm.com>

--- meta-openbmc-machines/meta-openpower/meta-ibm/meta-witherspoon/recipes-phosphor/chassis/vrm-control/ir35221-unbind-bind.sh
+++ meta-openbmc-machines/meta-openpower/meta-ibm/meta-witherspoon/recipes-phosphor/chassis/vrm-control/ir35221-unbind-bind.sh
@@ -3,7 +3,31 @@
 # Script to run on witherspoon BMC to unbind/bind the ir35221
 # driver's devices
 
-echo 4-0070 > /sys/bus/i2c/drivers/ir35221/$1
-echo 4-0071 > /sys/bus/i2c/drivers/ir35221/$1
-echo 5-0070 > /sys/bus/i2c/drivers/ir35221/$1
-echo 5-0071 > /sys/bus/i2c/drivers/ir35221/$1
+if [ "$1" = "unbind" ]
+then
+    if [ -e /sys/bus/i2c/drivers/ir35221/4-0070 ]
+    then
+        echo 4-0070 > /sys/bus/i2c/drivers/ir35221/$1
+    fi
+
+    if [ -e /sys/bus/i2c/drivers/ir35221/4-0071 ]
+    then
+        echo 4-0071 > /sys/bus/i2c/drivers/ir35221/$1
+    fi
+
+    if [ -e /sys/bus/i2c/drivers/ir35221/5-0070 ]
+    then
+        echo 5-0070 > /sys/bus/i2c/drivers/ir35221/$1
+    fi
+
+    if [ -e /sys/bus/i2c/drivers/ir35221/5-0071 ]
+    then
+        echo 5-0071 > /sys/bus/i2c/drivers/ir35221/$1
+    fi
+elif [ "$1" = "bind" ]
+then
+    echo 4-0070 > /sys/bus/i2c/drivers/ir35221/$1
+    echo 4-0071 > /sys/bus/i2c/drivers/ir35221/$1
+    echo 5-0070 > /sys/bus/i2c/drivers/ir35221/$1
+    echo 5-0071 > /sys/bus/i2c/drivers/ir35221/$1
+fi
