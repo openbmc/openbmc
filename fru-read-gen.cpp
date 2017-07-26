30ed237dfd18b09ce452ca386be71fa84442cad3
Marri Devender Rao <devenrao@in.ibm.com>
2017-07-26 02:14:11 -0500

    DO NOT MERGE - generated non host FRU map
    Change-Id: I452200a9a98279c164e44c484964381bf8b1f1e1
    Signed-off-by: Marri Devender Rao <devenrao@in.ibm.com>

---
+++ fru-read-gen.cpp
@@ -0,0 +1,255 @@
+// !!! WARNING: This is a GENERATED Code..Please do NOT Edit !!!
+#include <iostream>
+#include "fruread.hpp"
+
+extern const FruMap frus = {
+   {0,{
+
+         {"/system",{
+             {"xyz.openbmc_project.Inventory.Revision",{
+                 {"Version",{
+                     "Product",
+                     "Version",
+                     ""
+                 }},
+             }},
+             {"xyz.openbmc_project.Inventory.Decorator.Asset",{
+                 {"PartNumber",{
+                     "Product",
+                     "Part Number",
+                     ""
+                 }},
+                 {"SerialNumber",{
+                     "Product",
+                     "Serial Number",
+                     ""
+                 }},
+                 {"BuildDate",{
+                     "Product",
+                     "Mfg Date",
+                     ""
+                 }},
+                 {"Manufacturer",{
+                     "Product",
+                     "Manufacturer",
+                     ""
+                 }},
+             }},
+             {"xyz.openbmc_project.Inventory.Item",{
+                 {"PrettyName",{
+                     "Product",
+                     "Product Name",
+                     ""
+                 }},
+             }},
+        }},
+   }},
+   {1,{
+
+         {"/system/chassis/motherboard/dimm0",{
+             {"xyz.openbmc_project.Inventory.Revision",{
+                 {"Version",{
+                     "Product",
+                     "Version",
+                     ""
+                 }},
+             }},
+             {"xyz.openbmc_project.Inventory.Decorator.Asset",{
+                 {"PartNumber",{
+                     "Product",
+                     "Part Number",
+                     ""
+                 }},
+                 {"SerialNumber",{
+                     "Product",
+                     "Serial Number",
+                     ""
+                 }},
+                 {"BuildDate",{
+                     "Product",
+                     "Mfg Date",
+                     ""
+                 }},
+                 {"Manufacturer",{
+                     "Product",
+                     "Manufacturer",
+                     ""
+                 }},
+             }},
+             {"xyz.openbmc_project.Inventory.Item",{
+                 {"PrettyName",{
+                     "Product",
+                     "Product Name",
+                     ""
+                 }},
+             }},
+        }},
+   }},
+   {2,{
+
+         {"/system/chassis/motherboard/dimm1",{
+             {"xyz.openbmc_project.Inventory.Revision",{
+                 {"Version",{
+                     "Product",
+                     "Version",
+                     ""
+                 }},
+             }},
+             {"xyz.openbmc_project.Inventory.Decorator.Asset",{
+                 {"PartNumber",{
+                     "Product",
+                     "Part Number",
+                     ""
+                 }},
+                 {"SerialNumber",{
+                     "Product",
+                     "Serial Number",
+                     ""
+                 }},
+                 {"BuildDate",{
+                     "Product",
+                     "Mfg Date",
+                     ""
+                 }},
+                 {"Manufacturer",{
+                     "Product",
+                     "Manufacturer",
+                     ""
+                 }},
+             }},
+             {"xyz.openbmc_project.Inventory.Item",{
+                 {"PrettyName",{
+                     "Product",
+                     "Product Name",
+                     ""
+                 }},
+             }},
+        }},
+   }},
+   {3,{
+
+         {"/system/chassis/motherboard/cpu0",{
+             {"xyz.openbmc_project.Inventory.Decorator.Asset",{
+                 {"PartNumber",{
+                     "Board",
+                     "Part Number",
+                     ""
+                 }},
+                 {"SerialNumber",{
+                     "Board",
+                     "Serial Number",
+                     ""
+                 }},
+                 {"BuildDate",{
+                     "Board",
+                     "Mfg Date",
+                     ""
+                 }},
+                 {"Manufacturer",{
+                     "Board",
+                     "Manufacturer",
+                     ""
+                 }},
+             }},
+             {"xyz.openbmc_project.Inventory.Item",{
+                 {"PrettyName",{
+                     "Board",
+                     "Product Name",
+                     ""
+                 }},
+             }},
+        }},
+   }},
+   {4,{
+
+         {"/system/chassis/motherboard/cpu1",{
+             {"xyz.openbmc_project.Inventory.Decorator.Asset",{
+                 {"PartNumber",{
+                     "Board",
+                     "Part Number",
+                     ""
+                 }},
+                 {"SerialNumber",{
+                     "Board",
+                     "Serial Number",
+                     ""
+                 }},
+                 {"BuildDate",{
+                     "Board",
+                     "Mfg Date",
+                     ""
+                 }},
+                 {"Manufacturer",{
+                     "Board",
+                     "Manufacturer",
+                     ""
+                 }},
+             }},
+             {"xyz.openbmc_project.Inventory.Item",{
+                 {"PrettyName",{
+                     "Board",
+                     "Product Name",
+                     ""
+                 }},
+             }},
+        }},
+   }},
+   {5,{
+
+         {"/system/chassis/motherboard/fan1",{
+             {"xyz.openbmc_project.Inventory.Item",{
+                 {"PrettyName",{
+                     "Product",
+                     "Product Name",
+                     ""
+                 }},
+             }},
+        }},
+   }},
+   {6,{
+
+         {"/system/chassis/motherboard/boxelder/bmc",{
+             {"xyz.openbmc_project.Inventory.Decorator.Asset",{
+                 {"PartNumber",{
+                     "Board",
+                     "Part Number",
+                     ""
+                 }},
+                 {"SerialNumber",{
+                     "Board",
+                     "Serial Number",
+                     ""
+                 }},
+                 {"BuildDate",{
+                     "Board",
+                     "Mfg Date",
+                     ""
+                 }},
+                 {"Manufacturer",{
+                     "Board",
+                     "Manufacturer",
+                     ""
+                 }},
+             }},
+             {"xyz.openbmc_project.Inventory.Item",{
+                 {"PrettyName",{
+                     "Board",
+                     "Product Name",
+                     ""
+                 }},
+             }},
+        }},
+   }},
+   {7,{
+
+         {"/system/chassis/motherboard/boxelder/bmc/ethernet",{
+             {"xyz.openbmc_project.Inventory.Item",{
+                 {"PrettyName",{
+                     "Product",
+                     "Product Name",
+                     ""
+                 }},
+             }},
+        }},
+   }},
+};
