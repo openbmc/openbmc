e520df4806617df03146fe523fd5c308b5a6ac82
Matthew Barth <msbarth@us.ibm.com>
2018-12-14 15:57:46 -0600

    wspoon: Fix max31785 hwmon startup
    The max31785 hwmon instance fails to start after a systemd patch was
    removed to handle special characters in the device paths. This uses
    `systemd-escape` to escape and unescape the device path when the udev
    rule triggers the max31785 hwmon startup script.
    
    Resolves openbmc/openbmc#3443
    
    (From meta-ibm rev: 2790b22411920574b95111a44137be21e07a2e3a)
    
    Change-Id: I46d1d1a9de8cb740bfd57cebfd5b2474ff69246c
    Signed-off-by: Matthew Barth <msbarth@us.ibm.com>
    Signed-off-by: Brad Bishop <bradleyb@fuzziesquirrel.com>

--- meta-ibm/meta-witherspoon/recipes-phosphor/sensors/phosphor-hwmon/start_max31785_hwmon.sh
+++ meta-ibm/meta-witherspoon/recipes-phosphor/sensors/phosphor-hwmon/start_max31785_hwmon.sh
@@ -3,9 +3,9 @@
 # Copy in the correct MAX31785 phosphor-hwmon config file to use based on the
 # WaterCooled property, and then start the hwmon service.
 
-# $1: The OF_FULLNAME udev attribute for the MAX31785
+# $1: The escaped OF_FULLNAME udev attribute for the MAX31785
 
-base="/etc/default/obmc/hwmon/"$1
+base="/etc/default/obmc/hwmon/"$(systemd-escape -u "$1")
 target=$base".conf"
 
 service=$(mapper get-service /xyz/openbmc_project/inventory/system/chassis)
@@ -22,5 +22,5 @@ fi
 
 cp $source $target
 
-instance=$(systemd-escape $1)
+instance='-'$1
 systemctl start xyz.openbmc_project.Hwmon@$instance.service
