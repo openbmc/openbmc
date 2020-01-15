591abde64bc2e5805ea88f18e21cec4ee3f4e2ad
Vijay Khemka <vijaykhemka@fb.com>
2020-01-15 15:42:25 -0800

    meta-facebook: tiogapass: modify power control script
    Update power control scripts as per x86 host power control
    which is dbus based.
    
    (From meta-facebook rev: 14bcc378fa968fd8c8fbd4de3aa7b568c1899ab0)
    
    Change-Id: I36348f9e54781f377ff509cf9287443db5a78262
    Signed-off-by: Vijay Khemka <vijaykhemka@fb.com>
    Signed-off-by: Brad Bishop <bradleyb@fuzziesquirrel.com>

--- meta-facebook/meta-tiogapass/recipes-fbtp/fb-powerctrl/files/setup_gpio.sh
+++ meta-facebook/meta-tiogapass/recipes-fbtp/fb-powerctrl/files/setup_gpio.sh
@@ -14,25 +14,9 @@ function set_gpio_active_low() {
 GPIO_BASE=$(cat /sys/class/gpio/gpio*/base)
 
 # FM_BMC_READY_N, GPIO S1, active low
-set_gpio_active_low $((${GPIO_BASE} + 144 +1)) high
-
-# FM_BMC_SSB_SMI_LPC_N, GPIO Q6, active low
-#set_gpio_active_low $((${GPIO_BASE} + 128 + 6)) high
-
-# FP_PWR_BTN_PASS_R_N, GPIO E3, active low
-set_gpio_active_low $((${GPIO_BASE} + 32 + 3)) high
-
-# FP_PWR_GOOD, GPIO B6, active low
-set_gpio_active_low $((${GPIO_BASE} + 8 + 6)) in
-
-# SYSRST_BTN_OUT_N, GPIO E1, active low
-set_gpio_active_low $((${GPIO_BASE} + 32 + 1)) high
+set_gpio_active_low $((${GPIO_BASE} + 144 +1)) low
 
 # FP_PECI_MUX, active low
 set_gpio_active_low $((${GPIO_BASE} + 212)) high
-
-# Set PECI Mux to high to select Peci interface
-echo 1 > /sys/class/gpio/gpio$((${GPIO_BASE} + 212))/value
-
 
 exit 0;
