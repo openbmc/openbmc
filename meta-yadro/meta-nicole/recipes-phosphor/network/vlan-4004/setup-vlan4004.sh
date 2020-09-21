1c8460cd2311db9a6470e4b055905a030f59a5a1
Alexander Filippov <a.filippov@yadro.com>
2020-09-21 15:56:19 +0300

    nicole: Remove obsolete vlan-4004 package.
    The functionality of this package was added as a part of `systemd-conf`
    package. This package is no longer required.
    
    (From meta-yadro rev: e5fb11c84f7455acff17efb79373e954afef1780)
    
    Change-Id: Iee5368e0bcc60ccdb745323937ec5506ce3c1c4d
    Signed-off-by: Alexander Filippov <a.filippov@yadro.com>
    Signed-off-by: Andrew Geissler <geissonator@yahoo.com>

--- meta-yadro/meta-nicole/recipes-phosphor/network/vlan-4004/setup-vlan4004.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh -eu
-
-VLAN='VLAN=eth0.4004'
-ETH0_CFG='/etc/systemd/network/00-bmc-eth0.network'
-
-if ( ! grep -q "${VLAN}" ${ETH0_CFG} ); then
-    sed -i "/^\[Network\]$/a ${VLAN}" ${ETH0_CFG}
-fi
