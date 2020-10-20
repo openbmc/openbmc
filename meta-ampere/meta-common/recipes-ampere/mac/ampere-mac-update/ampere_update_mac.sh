ae305b639ad8d95c0b9e00c464f63b2930c47441
Chanh Nguyen <chnguyen@amperecomputing.com>
2020-10-20 17:51:44 +0700

    mtjade: Update BMC MAC address from FRU Inventory Information
    This commit supports to get BMC MAC address from FRU over the
    FRU Inventory Information (/xyz/openbmc_project/inventory/system/chassis/motherboard)
    then update eth0 MAC address and set the eth1addr u-boot variable.
    
    The ampere_update_mac.service is executed after
    xyz.openbmc_project.Inventory.Manager runs to make sure the Inventory was
    updated.
    
    Test case:
    
        1. Get MAC Addr (1) from /xyz/openbmc_project/inventory/system/chassis/motherboard
        2. Run "fw_printenv" to compare "eth1addr" with MAC Addr (1)
        3. Run "ip a" to compare with MAC Addr (1).
    
    Signed-off-by: Chanh Nguyen <chnguyen@amperecomputing.com>

---
+++ meta-ampere/meta-common/recipes-ampere/mac/ampere-mac-update/ampere_update_mac.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+#
+# This script is used to get the MAC Address from FRU Inventory information
+# Author : Hoang Nguyen  <hnguyen@amperecomputing.com>
+# Modify : Chanh Nguyen  <chnguyen@amperecomputing.com>
+
+ETHERNET_INTERFACE="eth0"
+ENV_ETH="eth1addr"
+ENV_MAC_ADDR=`fw_printenv`
+
+# Check if BMC MAC address is exported
+if [[ $ENV_MAC_ADDR =~ $ENV_ETH ]]; then
+    echo "WARNING: BMC MAC address is alread updated!"
+    exit 0
+fi
+
+# Read FRU Board Custom Field 1 to get the MAC address
+CUSTOM_FIELD_1=`busctl get-property xyz.openbmc_project.Inventory.Manager /xyz/openbmc_project/inventory/system/chassis/motherboard xyz.openbmc_project.Inventory.Item.NetworkInterface MACAddress`
+MAC_ADDR=`echo $CUSTOM_FIELD_1 | cut -d "\"" -f 2`
+
+# Check if BMC MAC address is exported
+if [ -z "${MAC_ADDR}" ]; then
+    echo "ERROR: No BMC MAC address is detected from FRU Inventory information!"
+    # Return 1 so that systemd knows the service failed to start
+    exit 1
+fi
+
+# Request to update the MAC address
+fw_setenv ${ENV_ETH} ${MAC_ADDR}
+
+if [[ $? -ne 0 ]]; then
+   echo "ERROR: fw_setenv failed"
+   exit 1
+fi
+
+# Request to restart the service
+ifconfig ${ETHERNET_INTERFACE} down
+ifconfig ${ETHERNET_INTERFACE} hw ether ${MAC_ADDR}
+ifconfig ${ETHERNET_INTERFACE} up
+
+if [[ $? -ne 0 ]]; then
+   echo "ERROR: Can not update MAC ADDR"
+   exit 1
+fi
+
+echo "Updated the MAC address!"
