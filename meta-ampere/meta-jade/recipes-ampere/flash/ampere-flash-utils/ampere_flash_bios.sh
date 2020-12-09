210af5c5927b0837c53f3c10ff4f2d640614cf12
Chanh Nguyen <chnguyen@amperecomputing.com>
2020-12-09 15:59:50 +0700

    meta-ampere: Improve flash bios script for pnor detection
    Currently, if the mux selection was not switched in booting,
    the SPI flash driver will not probe success. It can not
    update the bios firmware.
    
    This patch will update the bind/unbind method in case the kernel has
    not yet probed.
    
    Signed-off-by: Chanh Nguyen <chnguyen@amperecomputing.com>

--- meta-ampere/meta-jade/recipes-ampere/flash/ampere-flash-utils/ampere_flash_bios.sh
+++ meta-ampere/meta-jade/recipes-ampere/flash/ampere-flash-utils/ampere_flash_bios.sh
@@ -14,39 +14,90 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-do_flash_bios() {
-	BIOS_MTD=$(cat /proc/mtd | grep "pnor" | sed -n 's/^\(.*\):.*/\1/p')
-	BIOS_IMAGE="$1"
+do_flash () {
+	# Check the PNOR partition available
+	HOST_MTD=$(cat /proc/mtd | grep "pnor" | sed -n 's/^\(.*\):.*/\1/p')
+	if [ -z "$HOST_MTD" ];
+	then
+		# If the PNOR partition is not available, then bind again driver
+		echo "--- Bind the ASpeed SMC driver"
+		echo 1e630000.spi > /sys/bus/platform/drivers/aspeed-smc/bind
+		sleep 2
 
-	if [ ! -f $BIOS_IMAGE ]; then
-		echo $BIOS_IMAGE
-		echo "Cannot find the BIOS image file"
-		exit 1
+		HOST_MTD=$(cat /proc/mtd | grep "pnor" | sed -n 's/^\(.*\):.*/\1/p')
+		if [ -z "$HOST_MTD" ];
+		then
+			echo "Fail to probe Host SPI-NOR device"
+			exit 1
+		fi
+
+		echo "--- Flashing firmware to @/dev/$HOST_MTD"
+		flash_eraseall /dev/$HOST_MTD
+		flashcp -v $IMAGE /dev/$HOST_MTD
+
+		echo "--- Unbind the ASpeed SMC driver"
+		echo 1e630000.spi > /sys/bus/platform/drivers/aspeed-smc/unbind
+	else
+		echo "--- Flashing firmware to @/dev/$HOST_MTD"
+		flash_eraseall /dev/$HOST_MTD
+		flashcp -v $IMAGE /dev/$HOST_MTD
 	fi
-
-	echo "turning chassis off"
-	obmcutil chassisoff
-	sleep 20
-
-	# 226 is BMC_GPIOAC2_SPI0_PROGRAM_SEL
-	gpioset 0 226=0
-	# 227 is BMC_GPIOAC3_SPI0_BACKUP_SEL
-	gpioset 0 227=0
-
-	echo "Flashing BIOS @/dev/$BIOS_MTD"
-	flash_eraseall /dev/$BIOS_MTD
-	flashcp -v $BIOS_IMAGE /dev/$BIOS_MTD
-
-	# 227 is BMC_GPIOAC3_SPI0_BACKUP_SEL
-	gpioset 0 227=0
-	# 226 is BMC_GPIOAC2_SPI0_PROGRAM_SEL
-	gpioset 0 226=1
-
 }
+
 
 if [ $# -eq 0 ]; then
 	echo "Usage: $(basename $0) <BIOS image file>"
 	exit 0
 fi
 
-do_flash_bios $@
+IMAGE="$1"
+if [ ! -f $IMAGE ]; then
+	echo $IMAGE
+	echo "The image file $IMAGE does not exist"
+	exit 1
+fi
+
+# Turn off the Host if it is currently ON
+chassisstate=$(obmcutil chassisstate | awk -F. '{print $NF}')
+echo "--- Current Chassis State: $chassisstate"
+if [ "$chassisstate" == 'On' ];
+then
+	echo "--- Turning the Chassis off"
+	obmcutil chassisoff
+	sleep 10
+	# Check if HOST was OFF
+	chassisstate_off=$(obmcutil chassisstate | awk -F. '{print $NF}')
+	if [ "$chassisstate_off" == 'On' ];
+	then
+		echo "--- Error : Failed turning the Chassis off"
+		exit 1
+	fi
+fi
+
+# Switch the host SPI bus to BMC"
+echo "--- Switch the host SPI bus to BMC."
+gpioset 0 226=0
+
+if [[ $? -ne 0 ]]; then
+	echo "ERROR: Switch the host SPI bus to BMC. Please check gpio state"
+	exit 1
+fi
+
+# Flash the firmware
+do_flash
+
+# Switch the host SPI bus to HOST."
+echo "--- Switch the host SPI bus to HOST."
+gpioset 0 226=1
+
+if [[ $? -ne 0 ]]; then
+	echo "ERROR: Switch the host SPI bus to HOST. Please check gpio state"
+	exit 1
+fi
+
+if [ "$chassisstate" == 'On' ];
+then
+	sleep 5
+	echo "Turn on the Host"
+	obmcutil chassison
+fi
