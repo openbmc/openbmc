b452727aa7758d85e5b3e6b9dbe00ae8c56510c2
Chanh Nguyen <chnguyen@amperecomputing.com>
2020-12-11 16:00:59 +0700

    meta-ampere:meta-jade: Fix application name in hightemp/overtemp service
    Currently, the *hightemp*.service and *overtemp.service call wrong name
    of the tempevent_log app. It makes the LOG can't save into event log.
    
    This patch will revise the tempevent_log name in service file and use
    Ampere registries to save this log.

--- meta-ampere/meta-jade/recipes-ampere/host/tempevent-log/include/utils.hpp
+++ meta-ampere/meta-jade/recipes-ampere/host/tempevent-log/include/utils.hpp
@@ -121,29 +121,6 @@ static void toHexStr(const std::vector<uint8_t> &data, std::string &hexStr) {
 }
 
 template <typename... T>
-static uint16_t
-selAddRecord(const std::string &message, const int selPriority,
-             const std::string &path, const std::vector<uint8_t> &selData,
-             const bool &assert, const uint16_t &genId, T &&... metadata) {
-  // Only 3 bytes of SEL event data are allowed in a system record
-  if (selData.size() > selEvtDataMaxSize) {
-    throw std::invalid_argument("Event data too large");
-  }
-  std::string selDataStr;
-  toHexStr(selData, selDataStr);
-
-  unsigned int recordId = getNewRecordId();
-  sd_journal_send("MESSAGE=%s", message.c_str(), "PRIORITY=%i", selPriority,
-                  "MESSAGE_ID=%s", selMessageId, "IPMI_SEL_RECORD_ID=%d",
-                  recordId, "IPMI_SEL_RECORD_TYPE=%x", selSystemType,
-                  "IPMI_SEL_GENERATOR_ID=%x", genId, "IPMI_SEL_SENSOR_PATH=%s",
-                  path.c_str(), "IPMI_SEL_EVENT_DIR=%x", assert,
-                  "IPMI_SEL_DATA=%s", selDataStr.c_str(),
-                  std::forward<T>(metadata)..., NULL);
-  return recordId;
-}
-
-template <typename... T>
 static uint16_t selAddOemRecord(const std::string &message,
                                 const int selPriority,
                                 const std::vector<uint8_t> &selData,
