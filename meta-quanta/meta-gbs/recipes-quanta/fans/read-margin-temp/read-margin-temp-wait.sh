b3464d089a40bc67af0ee842f40930e5e3b4020c
George Hung <george.hung@quantatw.com>
2021-04-08 07:36:01 +0800

    meta-quanta: gbs: populated the JSON according to the installed drives
    Add the @nvmeList@ template and dynamically detect for which
    drives we should have and populated the JSON accordingly
    
    Signed-off-by: George Hung <george.hung@quantatw.com>
    Change-Id: Id0f6cefe80ae72dc8d3601d0a03dd1039341175c

--- meta-quanta/meta-gbs/recipes-quanta/fans/read-margin-temp/read-margin-temp-wait.sh
+++ meta-quanta/meta-gbs/recipes-quanta/fans/read-margin-temp/read-margin-temp-wait.sh
@@ -15,28 +15,36 @@ done
 
 nvmePath="/xyz/openbmc_project/sensors/temperature/nvme"
 nvmeInventoryPath="/xyz/openbmc_project/inventory/system/chassis/motherboard/nvme"
+nvmeList=""
 # Get and Set WCTEMP
 for ((i = 0; i < 16; i++)); do
     name="@WCTemp$(printf "%02d" $i)@"
     wcTemp=72000
-    presentState=$(busctl get-property \
+    presentState="$(busctl get-property \
         xyz.openbmc_project.Inventory.Manager \
         ${nvmeInventoryPath}${i} \
         xyz.openbmc_project.Inventory.Item \
-        Present | awk '{print $2}')
+        Present | awk '{print $2}')"
 
-    if [[ $presentState == "true" ]]; then
-        wcTemp=$(
+    if [[ "$presentState" == "true" ]]; then
+        wcTemp="$(
             busctl get-property xyz.openbmc_project.nvme.manager \
                 ${nvmePath}${i} \
                 xyz.openbmc_project.Sensor.Threshold.Critical \
                 CriticalHigh | awk '{print $2}'
-        )
-        wcTemp=$((wcTemp * 1000))
+        )"
+        wcTemp="$((wcTemp * 1000))"
+        if [[ -z "$nvmeList" ]]; then
+            nvmeList="\"nvme"${i}"\""
+        else
+            nvmeList="${nvmeList}"", \"nvme"${i}"\""
+        fi
     fi
 
-    sed -i "s/$name/${wcTemp}/g" $TEMP_FILE
+    sed -i "s/$name/${wcTemp}/g" "$TEMP_FILE"
 done
+
+sed -i "s/@nvmeList@/${nvmeList}/g" "$TEMP_FILE"
 
 # Use shell parameter expansion to trim the ".in" suffix
 mv "$TEMP_FILE" "${MARGIN_TABLE_FILE_IN%".in"}"
