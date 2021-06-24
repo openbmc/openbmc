openbmc/openbmc

commit a27a2f2675482180827748caa8a3d08f5ee726f2

libmctp: srcrev bump 507ba00dad..d4103f8f55
Andrew Jeffery (1):
      mctp-demux-daemon: Use systemd socket activation

Change-Id: Ied3f26b95370639b564aa7e1214e8d76bbc6074f
Signed-off-by: Andrew Geissler <openbmcbump-github@yahoo.com>

meta-phosphor/recipes-phosphor/mctp/mctp_git.bb
@@ -11,7 +11,7 @@ LICENSE = "Apache-2.0"
 LIC_FILES_CHKSUM = "file://LICENSE;md5=0d30807bb7a4f16d36e96b78f9ed8fae"
 SRC_URI = "git://github.com/openbmc/libmctp \
 	   file://default"
-SRCREV = "507ba00dadfa72b1a8e10c399b4fb3f3273c179d"
+SRCREV = "d4103f8f55e1f20869809b61be2567c48c817bb2"
 CONFFILES_${PN} = "${sysconfdir}/default/mctp"
 
 DEPENDS += "autoconf-archive-native \

commit 948c7b42589afefa044261b13ce7309cccd221d9

meta-phosphor: mctp: Add mctp-demux.socket
Resolves the following build error:

ERROR: mctp-1.0+git999-r1 do_package: QA Issue: mctp: Files/directories were installed but not shipped in any package:
  /lib/systemd/system/mctp-demux.socket

Fixes: a27a2f267548 ("libmctp: srcrev bump 507ba00dad..d4103f8f55")
Signed-off-by: George Liu <liuxiwei@inspur.com>
Change-Id: Ib302725bc54c312fc78caf00fa0fa5a2559668c5

meta-phosphor/recipes-phosphor/mctp/mctp_git.bb
@@ -15,9 +15,11 @@ SRCREV = "cfeaef8ecb0ab8c59ea4e145603523e599d60014"
 
 DEPENDS += "autoconf-archive-native \
             systemd \
-            "
+           "
 
-SYSTEMD_SERVICE_${PN} = "mctp-demux.service"
+SYSTEMD_SERVICE_${PN} = "mctp-demux.service \
+                         mctp-demux.socket \
+                        "
 
 PACKAGECONFIG ??= "${@bb.utils.filter('DISTRO_FEATURES', 'systemd', d)}"
 PACKAGECONFIG[systemd] = "--with-systemdsystemunitdir=${systemd_system_unitdir}, \