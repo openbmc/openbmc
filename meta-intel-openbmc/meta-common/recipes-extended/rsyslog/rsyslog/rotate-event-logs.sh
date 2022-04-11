e4b51dca811d7e003a72caf44d0963b8b0ac52b1
Zbigniew Kurzynski <zbigniew.kurzynski@intel.com>
2022-04-11 18:00:30 +0200

    meta-intel-openbmc: Use wildcard for rsyslog configs
    After this change the logrotate app will use and rotate
    all config files with extension .rsyslog instead of
    specific one. This should allow for easier configuration
    extension especially on down-stream projects.
    
    Tested: I manually created a new config file under
    location: /etc/logrotate.d/ with extension xx.rsyslog
    
    After calling this command
    
       /usr/sbin/logrotate -d /etc/logrotate.d/*.rsyslog
    
    I get following message, where first two entries regards
    original config files and the third one is my testing file.
    
       rotating pattern: /var/log/ipmi_sel
       …
       rotating pattern: /var/log/redfish
       …
       rotating pattern: /var/log/my_new_events
    
    Signed-off-by: Zbigniew Kurzynski <zbigniew.kurzynski@intel.com>
    Change-Id: Ic10c6eb211221291916e6ba6537ab9bf71efe178

--- meta-intel-openbmc/meta-common/recipes-extended/rsyslog/rsyslog/rotate-event-logs.sh
+++ meta-intel-openbmc/meta-common/recipes-extended/rsyslog/rsyslog/rotate-event-logs.sh
@@ -2,7 +2,7 @@
 
 while true; do
     sleep 60
-    /usr/sbin/logrotate /etc/logrotate.d/logrotate.rsyslog
+    /usr/sbin/logrotate /etc/logrotate.d/*.rsyslog
     ec=$?
     if [ $ec -ne 0 ] ; then
         echo "logrotate failed ($ec)"
