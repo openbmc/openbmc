openbmc/openbmc

commit 0998d1e4fc58e668ccf2ea5e07b86317b066160d

initramfs: Add init script for eMMC
This init script mounts the rootfs based on the root u-boot env
variable, mounts the read-write filesystem and sets up the overlay.

(From meta-phosphor rev: 86164b2e072cc6cebc9caf1614e2b1fa0e0884a0)

Change-Id: If9121048b6223d5391e5f6a8b7d6cd7d22707969
Signed-off-by: Adriana Kobylak <anoo@us.ibm.com>
Signed-off-by: Andrew Geissler <geissonator@yahoo.com>

meta-phosphor/recipes-phosphor/initrdscripts/phosphor-mmc-init/mmc-init.sh
@@ -1,0 +1,51 @@
+#!/bin/sh
+
+# Get the value of the root env variable found in /proc/cmdline
+get_root() {
+    local root="$(cat /proc/cmdline)"
+    root="${root##* root=PARTLABEL=}"
+    root="${root%% *}"
+    [ "${root}" != "" ] && echo "${root}"
+}
+
+fslist="proc sys dev run"
+rodir=/mnt/rofs
+cd /
+mkdir -p $fslist
+mount dev dev -tdevtmpfs
+mount sys sys -tsysfs
+mount proc proc -tproc
+mount tmpfs run -t tmpfs -o mode=755,nodev
+
+# Move the secondary GPT to the end of the device if needed. Look for the GPT
+# header signature "EFI PART" located 512 bytes from the end of the device.
+magic=$(tail -c 512 /dev/mmcblk0 | hexdump -C -n 8 | grep "EFI PART")
+if test -z "${magic}"; then
+    sgdisk -e /dev/mmcblk0
+    partprobe
+fi
+
+# There eMMC GPT labels for the rootfs are rofs-a and rofs-b, and the label for
+# the read-write partition is rwfs. Run udev to make the partition labels show 
+# up. Mounting by label allows for partition numbers to change if needed.
+udevd --daemon
+udevadm trigger --type=devices --action=add
+udevadm settle --timeout=10
+
+mkdir -p $rodir
+if ! mount /dev/disk/by-partlabel/"$(get_root)" $rodir -t ext4 -o ro; then
+    /bin/sh
+fi
+if ! mount /dev/disk/by-partlabel/rwfs $rodir/var -t ext4 -o rw; then
+    /bin/sh
+fi
+
+rm -rf $rodir/var/persist/etc-work/
+mkdir -p $rodir/var/persist/etc $rodir/var/persist/etc-work $rodir/var/persist/home/root
+mount overlay $rodir/etc -t overlay -o lowerdir=$rodir/etc,upperdir=$rodir/var/persist/etc,workdir=$rodir/var/persist/etc-work
+
+for f in $fslist; do
+    mount --move $f $rodir/$f
+done
+
+exec chroot $rodir /sbin/init

meta-phosphor/recipes-phosphor/images/obmc-phosphor-initramfs.bb
@@ -2,7 +2,11 @@ DESCRIPTION = "Small image capable of booting a device. The kernel includes \
 the Minimal RAM-based Initial Root Filesystem (initramfs), which finds the \
 first 'init' program more efficiently."
 
-PACKAGE_INSTALL = "${VIRTUAL-RUNTIME_base-utils} base-passwd ${ROOTFS_BOOTSTRAP_INSTALL} obmc-phosphor-initfs"
+# Init scripts
+INIT_PACKAGE = "obmc-phosphor-initfs"
+INIT_PACKAGE_df-phosphor-mmc = "phosphor-mmc-init"
+
+PACKAGE_INSTALL = "${VIRTUAL-RUNTIME_base-utils} base-passwd ${ROOTFS_BOOTSTRAP_INSTALL} ${INIT_PACKAGE}"
 
 # Do not pollute the initrd image with rootfs features
 IMAGE_FEATURES = "read-only-rootfs"

meta-phosphor/recipes-phosphor/initrdscripts/phosphor-mmc-init.bb
@@ -1,0 +1,26 @@
+SUMMARY = "Phosphor OpenBMC pre-init scripts for mmc"
+DESCRIPTION = "Phosphor OpenBMC filesystem mount implementation for mmc."
+LICENSE = "Apache-2.0"
+LIC_FILES_CHKSUM = "file://${COREBASE}/meta/files/common-licenses/Apache-2.0;md5=89aea4e17d99a7cacdbeed46a0096b10"
+
+PR = "r1"
+
+inherit allarch
+
+RDEPENDS_${PN} += " \
+    ${VIRTUAL-RUNTIME_base-utils} \
+    gptfdisk \
+    parted \
+    udev \
+"
+
+S = "${WORKDIR}"
+SRC_URI += "file://mmc-init.sh"
+
+do_install() {
+    install -m 0755 ${WORKDIR}/mmc-init.sh ${D}/init
+    install -d ${D}/dev
+    mknod -m 622 ${D}/dev/console c 5 1
+}
+
+FILES_${PN} += " /init /dev "

commit a2e2aea3873c100a4dbf27100c8bc337fbffe75a

meta-phosphor: phosphor-mmc-init: Fix commandline parsing for root
From Joel in [1]:

```
It expects a command line string to be in the form:

console=ttyS4,115200n8 rootwait root=PARTLABEL=rofs-a

If booting with root=PARTLABEL=rofs-a at the start of the command line
string (instead of the end), the parsing gets confused:

[    8.241229] /dev/disk/by-partlabel/root=PARTLABEL=rofs-a: Can't open blockdev
mount: mounting /dev/disk/by-partlabel/root=PARTLABEL=rofs-a on /mnt/rofs failed: No such file or directory

Similarly if the partition is not set, it gets confused:

[    1.919816] Run /init as init process
Starting version 251.8+
[    8.219396] /dev/disk/by-partlabel/rootwait: Can't open blockdev
mount: mounting /dev/disk/by-partlabel/rootwait on /mnt/rofs failed: No such file or directory

We should fix get_root so that it checks for the existance of
root=PARTLABEL, and matches on it. We could fall back on rofs-a if it
can't be found?
```

Based on Joel's example I tested the new implementation as follows:

```
$ declare -f old_get_root
old_get_root ()
{
    local root="$@";
    root="${root##* root=PARTLABEL=}";
    root="${root%% *}";
    [ "${root}" != "" ] && echo "${root}"
}
$ declare -f new_get_root
new_get_root ()
{
    local cmdline="$@";
    root=;
    for opt in $cmdline;
    do
        case $opt in
            root=PARTLABEL=*)
                root=${opt##root=PARTLABEL=}
            ;;
            *)

            ;;
        esac;
    done;
    [ -n "$root" ] && echo $root
}
```

Comparing the two under various commandline orderings:

```
$ echo $working_cmdline
console=ttyS4,115200n8 rootwait root=PARTLABEL=rofs-a
$ old_get_root $working_cmdline
rofs-a
$ new_get_root $working_cmdline
rofs-a
$
```

```
$ echo $broken_cmdline
root=PARTLABEL=rofs-a console=ttyS4,115200n8 rootwait
$ old_get_root $broken_cmdline
root=PARTLABEL=rofs-a
$ new_get_root $broken_cmdline
rofs-a
$
```

```
$ echo $maybe_cmdline
console=ttyS4,115200n8 root=PARTLABEL=rofs-a rootwait
$ old_get_root $maybe_cmdline
rofs-a
$ new_get_root $maybe_cmdline
rofs-a
$
```

```
$ echo $dev_cmdline
console=ttyS4,115200n8 root=/dev/sda1 rootwait
$ old_get_root $dev_cmdline
console=ttyS4,115200n8
$ new_get_root $dev_cmdline
$
```

In each case new_get_root() gives the expected output.

[1]: https://github.com/openbmc/openbmc/issues/3898

Fixes: 0998d1e4fc58 ("initramfs: Add init script for eMMC")

meta-phosphor/recipes-phosphor/initrdscripts/phosphor-mmc-init/mmc-init.sh
@@ -2,10 +2,19 @@
 
 # Get the value of the root env variable found in /proc/cmdline
 get_root() {
-    local root="$(cat /proc/cmdline)"
-    root="${root##* root=PARTLABEL=}"
-    root="${root%% *}"
-    [ "${root}" != "" ] && echo "${root}"
+    local cmdline="$(cat /proc/cmdline)"
+    root=
+    for opt in $cmdline
+    do
+        case $opt in
+            root=PARTLABEL=*)
+                root=${opt##root=PARTLABEL=}
+                ;;
+            *)
+                ;;
+        esac
+    done
+    [ -n "$root" ] && echo $root
 }
 
 fslist="proc sys dev run"