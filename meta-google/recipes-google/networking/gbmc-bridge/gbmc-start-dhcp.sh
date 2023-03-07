66f1e128a014e1e29b2f6b40ad33bf9e6dcdc3da
Yuxiao Zhang <yuxiaozhang@google.com>
2023-03-07 13:51:25 -0800

    meta-google: Netboot: Provide command to trigger dhcp
    Installer needs to trigger dhcp process when
    netboot starts. Currently it just powercycles
    the machine, which is not efficient.
    This provides a script that installer can
    invoke without powercycle machine.
    
    Tested: tested manually on the machine and
    verified that dhcp has started.
    
    Change-Id: I6e596e3695f88543ad864eb431587a5f974f81b4
    Signed-off-by: Yuxiao Zhang <yuxiaozhang@google.com>

---
+++ meta-google/recipes-google/networking/gbmc-bridge/gbmc-start-dhcp.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+# Copyright 2023 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# stop dhcp term service to prevent race condition
+systemctl is-active --quiet gbmc-br-dhcp-term && systemctl stop gbmc-br-dhcp-term
+
+# start the dhcp service
+systemctl start gbmc-br-dhcp
