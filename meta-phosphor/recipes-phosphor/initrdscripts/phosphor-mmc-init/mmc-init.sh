openbmc/openbmc

commit 2345ace954c2db96da60b871735281a1bb4fa374

meta-phosphor: phosphor-mmc-init: fix shellcheck
Signed-off-by: Patrick Williams <patrick@stwcx.xyz>
Change-Id: I1f668e1e40c937e016c60af13705ea60936ed739

meta-phosphor/recipes-phosphor/initrdscripts/phosphor-mmc-init/mmc-init.sh
@@ -2,9 +2,9 @@
 
 # Get the value of the root env variable found in /proc/cmdline
 get_root() {
-    local cmdline="$(cat /proc/cmdline)"
+    _cmdline="$(cat /proc/cmdline)"
     root=
-    for opt in $cmdline
+    for opt in $_cmdline
     do
         case $opt in
             root=PARTLABEL=*)
@@ -14,7 +14,7 @@ get_root() {
                 ;;
         esac
     done
-    [ -n "$root" ] && echo $root
+    [ -n "$root" ] && echo "$root"
 }
 
 fslist="proc sys dev run"
@@ -23,7 +23,7 @@ mmcdev="/dev/mmcblk0"
 rwfsdev="/dev/disk/by-partlabel/rwfs"
 
 cd /
-mkdir -p $fslist
+mkdir -p "$fslist"
 mount dev dev -tdevtmpfs
 mount sys sys -tsysfs
 mount proc proc -tproc
@@ -48,7 +48,7 @@ if ! tail -c 512 "${mmcdev}" | hexdump -C -n 8 | grep -q "EFI PART"; then
 fi
 
 # There eMMC GPT labels for the rootfs are rofs-a and rofs-b, and the label for
-# the read-write partition is rwfs. Run udev to make the partition labels show 
+# the read-write partition is rwfs. Run udev to make the partition labels show
 # up. Mounting by label allows for partition numbers to change if needed.
 udevd --daemon
 udevadm trigger --type=devices --action=add
@@ -62,25 +62,24 @@ fi
 # Determine if a factory reset has been requested
 mkdir -p /var/lock
 resetval=$(fw_printenv -n rwreset 2>/dev/null)
-gpiopresent=$(gpiofind factory-reset-toggle)
-if [ $? -eq 0 ]; then
-    gpioval=$(gpioget $gpiopresent)
+if gpiopresent=$(gpiofind factory-reset-toggle) ; then
+    gpioval=$(gpioget "$gpiopresent")
 else
     gpioval=""
 fi
 # Prevent unnecessary resets on first boot
-if [ -n "$gpioval" -a -z "$resetval" ]; then
-    fw_setenv rwreset $gpioval
+if [ -n "$gpioval" ] && [ -z "$resetval" ]; then
+    fw_setenv rwreset "$gpioval"
     resetval=$gpioval
 fi
-if [ "$resetval" = "true" -o -n "$gpioval" -a "$resetval" != "$gpioval" ]; then
+if [ "$resetval" = "true" ] || [ -n "$gpioval" ] && [ "$resetval" != "$gpioval" ]; then
     echo "Factory reset requested."
     if ! mkfs.ext4 -F "${rwfsdev}"; then
         echo "Reformat for factory reset failed."
         /bin/sh
     else
         # gpioval will be an empty string if factory-reset-toggle was not found
-        fw_setenv rwreset $gpioval
+        fw_setenv rwreset "$gpioval"
         echo "rwfs has been formatted."
     fi
 fi
@@ -95,7 +94,7 @@ mkdir -p $rodir/var/persist/etc $rodir/var/persist/etc-work $rodir/var/persist/h
 mount overlay $rodir/etc -t overlay -o lowerdir=$rodir/etc,upperdir=$rodir/var/persist/etc,workdir=$rodir/var/persist/etc-work
 
 for f in $fslist; do
-    mount --move $f $rodir/$f
+    mount --move "$f" "$rodir/$f"
 done
 
 exec switch_root $rodir /sbin/init

meta-phosphor/scripts/run-repotest
@@ -145,7 +145,6 @@ meta-openpower/recipes-phosphor/occ/openpower-occ-control/occ-active.sh
 meta-phosphor/recipes-phosphor/initrdscripts/files/obmc-init.sh
 meta-phosphor/recipes-phosphor/initrdscripts/files/obmc-shutdown.sh
 meta-phosphor/recipes-phosphor/initrdscripts/files/obmc-update.sh
-meta-phosphor/recipes-phosphor/initrdscripts/phosphor-mmc-init/mmc-init.sh
 meta-phosphor/recipes-support/nss-pam-ldapd/files/nslcd.init
 meta-quanta/meta-common/recipes-quanta/network/usb-network/usb-network.sh
 meta-quanta/meta-gbs/recipes-gbs/gbs-bios-update/files/bios-update.sh

commit 2ea84ebfbe65e2d66db22b459e92c9574a31a096

meta-phosphor: mmc-init: Fix creation of critical directories
`mkdir $fslist` is responsible for creating all the critical mount-point
directories for a functioning Linux userspace, such as `/dev`, `/proc`
and `/sys`.  2345ace954c2 ("meta-phosphor: phosphor-mmc-init: fix
shellcheck") prevented their existence by instead creating a single
directory named "proc sys dev run":

```
~ # ls -1
bin
dev
etc
init
lib
mnt
proc sys dev run
root
run
sbin
usr
var
```

The lack of directories lead to the inability to mount the required
filesystems, with the boot process stalling at an initrd shell prompt:

```
[    1.224302] Freeing initrd memory: 3272K
[    1.265274] Freeing unused kernel image (initmem) memory: 1024K
[    1.291134] Checked W+X mappings: passed, no W+X pages found
[    1.291492] Run /init as init process
mount: mounting sys on sys failed: No such file or directory
mount: mounting proc on proc failed: No such file or directory
[    2.600405]  mmcblk0: p1 p2 p3 p4 p5 p6 p7
The operation has completed successfully.
Failed to parse kernel command line, ignoring: No such file or directory
Starting systemd-udevd version 253.1^
Running in chroot, ignoring request.
Running in chroot, ignoring request.
cat: can't open '/proc/cmdline': No such file or directory
[    2.706024] /dev/disk/by-partlabel/: Can't open blockdev
mount: mounting /dev/disk/by-partlabel/ on /mnt/rofs failed: No such file or directory
/bin/sh: can't access tty; job control turned off
~ #
```

Change-Id: Ie0821508ed7e141c89d7620a6ed0adc069f5c267
Fixes: 2345ace954c2 ("meta-phosphor: phosphor-mmc-init: fix shellcheck")
Signed-off-by: Andrew Jeffery <andrew@aj.id.au>

meta-phosphor/recipes-phosphor/initrdscripts/phosphor-mmc-init/mmc-init.sh
@@ -23,7 +23,13 @@ mmcdev="/dev/mmcblk0"
 rwfsdev="/dev/disk/by-partlabel/rwfs"
 
 cd /
-mkdir -p "$fslist"
+
+# We want to make all the directories in $fslist, not one directory named by
+# concatonating the names with spaces
+#
+# shellcheck disable=SC2086
+mkdir -p $fslist
+
 mount dev dev -tdevtmpfs
 mount sys sys -tsysfs
 mount proc proc -tproc