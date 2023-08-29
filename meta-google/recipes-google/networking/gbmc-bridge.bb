openbmc/openbmc

commit bf4e27c3408955c214000108001e92c308c1861f

meta-google: gbmc-bridge: Deprecate hardcoded addresses
These addresses should not be used by default for traffic on the
interface, as we may have multiple BMCs using the same hardcoded address
WRT legacy applications. We want multiple BMCs of the same type to be
able to coexist if they have unique addresses and right now they are not
correctly preferring these addresses.

Change-Id: I0d02cbf63b54f2464ae889ea773e032da0e9b650
Signed-off-by: William A. Kennington III <wak@google.com>

meta-google/recipes-google/networking/gbmc-bridge.bb
@@ -97,7 +97,8 @@ do_install() {
 
   if [ ! -z "${GBMC_BR_MAC_ADDR}" ]; then
     sfx='${@mac_to_eui64(GBMC_BR_MAC_ADDR)}'
-    addr="Address=${GBMC_ULA_PREFIX}:$sfx/64\nAddress=fe80::$sfx/64"
+    addr="[Address]\nAddress=${GBMC_ULA_PREFIX}:$sfx/64\nPreferredLifetime=0\n"
+    addr+="[Address]\nAddress=fe80::$sfx/64\nPreferredLifetime=0"
     sed -i "s,@ADDR@,$addr," ${WORKDIR}/-bmc-gbmcbr.network.in
   else
     sed -i '/@ADDR@/d' ${WORKDIR}/-bmc-gbmcbr.network.in

meta-google/recipes-google/networking/gbmc-bridge/-bmc-gbmcbr.network.in
@@ -1,13 +1,13 @@
 [Match]
 Name=gbmcbr
 [Network]
-@ADDR@
 DHCP=false
 IPv6AcceptRA=true
 LLMNR=true
 MulticastDNS=true
 LinkLocalAddressing=ipv6
 IPv6PrefixDelegation=yes
+@ADDR@
 [IPv6AcceptRA]
 DHCPv6Client=false
 RouteMetric=1056

commit 3f9c98d8215850c639679112c7aff9710ea0e709

meta-google: gbmc-bridge: Fix bashism
The previous change introduced a bashism, but yocto expects that all
shell scripting is POSIX compatible.

Fixes: bf4e27c3408955c214000108001e92c308c1861f
Change-Id: Ifc3cce4d625a876b224853b3b04b1b84b95327b2
Signed-off-by: William A. Kennington III <wak@google.com>

meta-google/recipes-google/networking/gbmc-bridge.bb
@@ -98,7 +98,7 @@ do_install() {
   if [ ! -z "${GBMC_BR_MAC_ADDR}" ]; then
     sfx='${@mac_to_eui64(GBMC_BR_MAC_ADDR)}'
     addr="[Address]\nAddress=${GBMC_ULA_PREFIX}:$sfx/64\nPreferredLifetime=0\n"
-    addr+="[Address]\nAddress=fe80::$sfx/64\nPreferredLifetime=0"
+    addr="$addr[Address]\nAddress=fe80::$sfx/64\nPreferredLifetime=0"
     sed -i "s,@ADDR@,$addr," ${WORKDIR}/-bmc-gbmcbr.network.in
   else
     sed -i '/@ADDR@/d' ${WORKDIR}/-bmc-gbmcbr.network.in