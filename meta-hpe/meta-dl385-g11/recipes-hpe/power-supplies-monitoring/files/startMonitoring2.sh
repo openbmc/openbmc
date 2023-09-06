1ab2885884864713c373b765b1b371da81622d7c
Andrew Neyland <andrew.neyland@hpe.com>
2023-09-06 14:53:57 -0500

    meta-hpe: dl385-g11 power supplies monitoring
    Change-Id: I081bba56bc4a9c50044aa5a1625a8d586b4bea8a
    Signed-off-by: Andrew Neyland <andrew.neyland@hpe.com>

---
+++ meta-hpe/meta-dl385-g11/recipes-hpe/power-supplies-monitoring/files/startMonitoring2.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+#!/bin/sh
+myaddress=$(gpiofind PSU2_INST)
+state=$(gpioget "$myaddress")
+if [ "r$state" = "r1" ]
+then
+        psus-manager 2 up
+else
+        psus-manager 2 down
+fi
+/usr/bin/phosphor-multi-gpio-monitor --config /usr/share/gpios/psu2.json
