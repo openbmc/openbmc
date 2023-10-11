e71e7778d61b38cde9e2bef1ab72320a45cc2b2d
Joseph Reynolds <joseph-reynolds@charter.net>
2023-10-11 12:17:03 -0500

    dropbear SSH server: update security options
    This updates the algorithms offered by the BMC's dropbear SSH server
    when making new connections.
    
    The options.patch file is deleted because it is obsolete.  The
    localoptions.h file is updated to OpenBMC project preferences.  For
    background, see the Dropbear project default_options.h file.
    
    Tested: Validate intended algorithms via command: ssh -vv
    - Before:
    debug2: host key algorithms: rsa-sha2-256,ssh-rsa
    debug2: MACs ctos: hmac-sha1,hmac-sha2-256,hmac-sha2-512
    - After:
    debug2: host key algorithms: rsa-sha2-256
    debug2: MACs ctos: hmac-sha2-256,hmac-sha2-512
    
    Change-Id: I0eccb572c61ce3a800d2a189e9d84eb36ff592e7
    Signed-off-by: Joseph Reynolds <joseph-reynolds@charter.net>

--- meta-phosphor/recipes-core/dropbear/dropbear/localoptions.h
+++ meta-phosphor/recipes-core/dropbear/dropbear/localoptions.h
@@ -1,9 +1,11 @@
-// Disable CBC ciphers for modern security.
-#define DROPBEAR_ENABLE_CBC_MODE 0
+// Customizations.  See dropbear project default_options.h
 
 // Disable Chacha20-Poly1305 cipher.
 #define DROPBEAR_CHACHA20POLY1305 0
 
-#define DROPBEAR_SHA1_96_HMAC 0
-#define DROPBEAR_SHA2_256_HMAC 1
+#define DROPBEAR_SHA1_HMAC 0
 #define DROPBEAR_SHA2_512_HMAC 1
+
+#define DROPBEAR_RSA_SHA1 0
+
+#define DROPBEAR_DH_GROUP14_SHA1 0
