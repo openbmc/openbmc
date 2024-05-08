db8c93a5d87de93d46e5938dda306f3a9e02fdf0
Delphine CC Chiu <Delphine_CC_Chiu@wiwynn.com>
2024-05-08 14:18:23 +0800

    meta-facebook: yosemite4: Revise to 0 base to set NIC's EID
    The script to setup NICs' EID has changed to 0 base by a commit,
    the commit's hash: 178a2a2b4c1060abc4bea5f01042d6e8a67f4315.
    
    Change this setup script to fit that change. Also let the script
    call the service instead of the shell script.
    
    Change-Id: Ic4d660c8226e895de6eb4dfd4a6bd3788d4f1935
    Signed-off-by: Delphine CC Chiu <Delphine_CC_Chiu@wiwynn.com>

--- meta-facebook/meta-yosemite4/recipes-networking/mctp/files/setup-static-endpoints.sh
+++ meta-facebook/meta-yosemite4/recipes-networking/mctp/files/setup-static-endpoints.sh
@@ -8,23 +8,23 @@
 gpio_val=$(devmem 0x1e780088 8)
 # PRSNT_NIC0 is at the 6th bit (GPIOU5), 0 means NIC is present
 if [ $((gpio_val & 0x20)) -eq 0 ]; then
-    /usr/bin/configure-nic-mctp-endpoint.sh add 1
+    systemctl start setup-nic-endpoint-slot@0.service
 fi
 
 # Setup slot2 & 3 NIC
 gpio_val=$(devmem 0x1e780020 8)
 # PRSNT_NIC1 is at the 1th bit (GPIOE0), 0 means NIC is present
 if [ $((gpio_val & 0x01)) -eq 0 ]; then
-    /usr/bin/configure-nic-mctp-endpoint.sh add 2
+    systemctl start setup-nic-endpoint-slot@1.service
 fi
 # PRSNT_NIC2 is at the 2th bit (GPIOE1), 0 means NIC is present
 if [ $((gpio_val & 0x02)) -eq 0 ]; then
-    /usr/bin/configure-nic-mctp-endpoint.sh add 3
+    systemctl start setup-nic-endpoint-slot@2.service
 fi
 
 # Setup slot4 NIC
 gpio_val=$(devmem 0x1e780078 8)
 # PRSNT_NIC3 is at the 4th bit (GPIOM3), 0 means NIC is present
 if [ $((gpio_val & 0x08)) -eq 0 ]; then
-    /usr/bin/configure-nic-mctp-endpoint.sh add 4
+    systemctl start setup-nic-endpoint-slot@3.service
 fi
