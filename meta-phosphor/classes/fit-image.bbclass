openbmc/openbmc

commit 650d9dcf83b8c46a7f26cad093bfcb252dad3ae9

meta-phosphor: fit-image.bbclass: support compress kernel image
Extract relevant functions from upstream's kernel-uboot.bbclass
in order to be able to support compress kernel image.

Signed-off-by: James Chiang <cpchiang1@nuvoton.com>
Signed-off-by: Tim Lee <timlee660101@gmail.com>
Change-Id: I74c144e2d60d8276b74dc45767b88ff174e11937

meta-phosphor/classes/fit-image.bbclass
@@ -6,6 +6,9 @@ CONVERSION_CMD:fitImage = "run_assemble_fitimage ${IMAGE_NAME}.${type}"
 INITRAMFS_IMAGE="${IMAGE_NAME}.cpio.${INITRAMFS_CTYPE}"
 KERNEL_OUTPUT_DIR="${DEPLOY_DIR_IMAGE}"
 
+FIT_KERNEL_COMP_ALG ?= "none"
+FIT_KERNEL_COMP_ALG_EXTENSION ?= ""
+
 do_image_cpio[depends] += "virtual/kernel:do_deploy"
 
 run_assemble_fitimage() {
@@ -23,6 +26,16 @@ run_assemble_fitimage() {
 UBOOT_MKIMAGE_KERNEL_TYPE ?= "kernel"
 uboot_prep_kimage() {
     cp ${DEPLOY_DIR_IMAGE}/${KERNEL_IMAGETYPE} linux.bin
+
+    if [ "${linux_comp}" != "none" ] ; then
+        linux_suffix="${FIT_KERNEL_COMP_ALG_EXTENSION}"
+        if [ "${linux_comp}" = "gzip" ] ; then
+            gzip -9 linux.bin
+        elif [ "${linux_comp}" = "lzo" ] ; then
+            lzop -9 linux.bin
+        fi
+        mv -f "linux.bin${linux_suffix}" linux.bin
+    fi
 }
 
 DEPENDS:append = " u-boot-tools-native dtc-native virtual/${TARGET_PREFIX}binutils"

commit 5cadc795aa88c36dc7155153204cc9a7bbc3504d

fit-image.bbclass: correct compress algorithm for kernel image
Change default `linux_comp` from `none` to `${FIT_KERNEL_COMP_ALG}`

Fixes: 650d9dc
("meta-phosphor: fit-image.bbclass: support compress kernel image")

Signed-off-by: James Chiang <cpchiang1@nuvoton.com>
Signed-off-by: Tim Lee <timlee660101@gmail.com>
Change-Id: Ib4baba203219ead75f7f3f0d1b082301f2e01ac1

meta-phosphor/classes/fit-image.bbclass
@@ -12,7 +12,7 @@ FIT_KERNEL_COMP_ALG_EXTENSION ?= ""
 do_image_cpio[depends] += "virtual/kernel:do_deploy"
 
 run_assemble_fitimage() {
-    export linux_comp="none"
+    export linux_comp="${FIT_KERNEL_COMP_ALG}"
     fitimage_assemble $1.its $1.fitImage 1
 
     # The fitimage_assemble puts the image into DEPLOY_DIR_NAME due to