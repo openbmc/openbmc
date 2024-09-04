1440d6b0c59baa2a3bfca6c512fa39c2b840bc18
Patrick Williams <patrick@stwcx.xyz>
2024-09-04 16:36:00 -0400

    meta-hpe: squash latest shellcheck warnings
    The latest shellcheck introduced new warnings in meta-hpe.  Squash
    them with the disable syntax as they seem innocuous.
    
    Signed-off-by: Patrick Williams <patrick@stwcx.xyz>
    Change-Id: Ie160e02178b70b6025a964569a11a4778c526608

--- meta-hpe/meta-dl360-g11/recipes-hpe/power-sequencing/files/ipmi-power-button-monitoring.sh
+++ meta-hpe/meta-dl360-g11/recipes-hpe/power-sequencing/files/ipmi-power-button-monitoring.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
+# shellcheck disable=SC3061
 dbus-monitor --system --profile "type='method_call',path='/xyz/openbmc_project/state/host0',interface='org.freedesktop.DBus.Properties',member='Set'" | awk '/xyz.openbmc_project.State.Host/ { print "DONE" }' | while read -r; do
 /usr/bin/dl360Start.sh
 done
