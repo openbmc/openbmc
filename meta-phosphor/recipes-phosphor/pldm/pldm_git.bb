openbmc/openbmc

commit df196d5187832618afcd39c8dc35b322bc46e8b7

pldm: split pldmtool to separate package
Some system vendors may want to either:
    - only install pldmtool
    - prohibit installation of pldmtool

Split the pldm recipe so that it also generates a pldmtool sub-package,
which by default is still installed (via RRECOMMENDS) but can be
separately installed or explicitly prohibited.

Tested: Manually checked packages-split on pldm to confirm pldmtool
is placed in a separate package after build.  Built Yosemite4 and
confirmed pldmtool is still present.

Signed-off-by: Patrick Williams <patrick@stwcx.xyz>
Change-Id: I01fa9bd58de7bd1b9f65584ab61427ea74fe3bfb

meta-phosphor/recipes-phosphor/pldm/pldm_git.bb
@@ -17,9 +17,14 @@ DEPENDS += "phosphor-logging"
 PV = "1.0+git${SRCPV}"
 PR = "r1"
 
+PACKAGE_BEFORE_PN:append = " pldmtool"
+RRECOMMENDS:${PN}:append = "pldmtool"
+
 S = "${WORKDIR}/git"
 SYSTEMD_SERVICE:${PN} += "pldmd.service"
 SYSTEMD_SERVICE:${PN} += "pldmSoftPowerOff.service"
+
+FILES:pldmtool = "${bindir}/pldmtool"
 
 inherit meson pkgconfig
 inherit systemd

commit f48b96648099f3db7e73929a64ab9ebff77cc248

pldm: split libraries to separate packages
A previous commit attempted to split pldmtool into its own package,
which it did, but there are libraries provided by the pldm package
that are linked against by pldmtool.  Thus, the dependency between
pldmtool and pldm was not truly broken.  Split the libraries out
into yet another package ("pldm-libs") so that both pldmd and pldmtool
can link against the libraries separate from each other.

Fixes: df196d518783 ("pldm: split pldmtool to separate package")
Signed-off-by: Patrick Williams <patrick@stwcx.xyz>
Change-Id: Ia6a510b1d1d3c424200076226e55900f6e459e02

meta-phosphor/recipes-phosphor/pldm/pldm_git.bb
@@ -17,7 +17,7 @@ DEPENDS += "phosphor-logging"
 PV = "1.0+git${SRCPV}"
 PR = "r1"
 
-PACKAGE_BEFORE_PN:append = " pldmtool"
+PACKAGE_BEFORE_PN:append = " pldmtool pldm-libs"
 RRECOMMENDS:${PN}:append = "pldmtool"
 
 S = "${WORKDIR}/git"
@@ -25,6 +25,7 @@ SYSTEMD_SERVICE:${PN} += "pldmd.service"
 SYSTEMD_SERVICE:${PN} += "pldmSoftPowerOff.service"
 
 FILES:pldmtool = "${bindir}/pldmtool"
+FILES:pldm-libs = "${libdir}/lib*${SOLIBS}"
 
 inherit meson pkgconfig
 inherit systemd