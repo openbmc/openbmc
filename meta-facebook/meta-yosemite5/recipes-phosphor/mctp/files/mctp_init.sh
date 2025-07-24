433426e5d03acec199e40557f9d10876cef5a39c
Kevin Tung <kevin.tung@quantatw.com>
2025-07-24 14:45:12 +0800

    meta-facebook: yosemite5: add MCTP setup services for PLDM support
    Add services to set up MCTP link and assign static EIDs on the
    Yosemite5 platform. This enables PLDM over MCTP over I2C for sensor
    readings and firmware update functionality.
    
    Tested on Yosemite5 with the following command to verify MCTP setup:
    ```
    root@bmc:~# busctl tree au.com.codeconstruct.MCTP1
    └─ /au
      └─ /au/com
        └─ /au/com/codeconstruct
          └─ /au/com/codeconstruct/mctp1
            ├─ /au/com/codeconstruct/mctp1/interfaces
            │ ├─ /au/com/codeconstruct/mctp1/interfaces/lo
            │ └─ /au/com/codeconstruct/mctp1/interfaces/mctpi2c4
            └─ /au/com/codeconstruct/mctp1/networks
              └─ /au/com/codeconstruct/mctp1/networks/1
                └─ /au/com/codeconstruct/mctp1/networks/1/endpoints
                  ├─ /au/com/codeconstruct/mctp1/networks/1/endpoints/10
                  └─ /au/com/codeconstruct/mctp1/networks/1/endpoints/8
    ```
    
    Change-Id: I7e63bb7c6e93eced374975302f7733488289f16a
    Signed-off-by: Kevin Tung <kevin.tung.openbmc@gmail.com>

---
+++ meta-facebook/meta-yosemite5/recipes-phosphor/mctp/files/mctp_init.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+LOCAL_EID=8
+
+is_interface_up() {
+    ip link show "$1" | grep -q ".*UP.*"
+}
+
+is_eid_assigned() {
+    local iface="$1"
+    local eid="$2"
+    mctp addr show "$iface" 2>/dev/null | grep -q "eid $eid"
+}
+
+get_mctp_i2c_interfaces() {
+    for dev in /sys/class/net/mctpi2c*; do
+        [ -e "$dev" ] || continue
+        basename "$dev"
+    done
+}
+
+setup_mctp_interface() {
+    local iface="$1"
+
+    if is_interface_up "$iface"; then
+        echo "$iface is already up, skipping"
+    else
+        echo "Setting up $iface"
+        mctp link set "$iface" up
+    fi
+
+    if is_eid_assigned "$iface" "$LOCAL_EID"; then
+        echo "EID $LOCAL_EID already assigned to $iface, skipping"
+    else
+        echo "Assigning EID $LOCAL_EID to $iface"
+        mctp addr add "$LOCAL_EID" dev "$iface"
+    fi
+}
+
+for iface in $(get_mctp_i2c_interfaces); do
+    setup_mctp_interface "$iface"
+done
+
+echo "MCTP init complete"
---
+++ meta-facebook/meta-yosemite5/recipes-phosphor/mctp/files/mctp_setup.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+
+dev="${1:-}"
+
+# Mapping table: "devname:iface:physaddr:eid"
+declare -a endpoint_map=(
+    "nic_mctp:mctpi2c4:0x32:10"
+    "cxl_mctp:mctpi2c12:0x33:20"
+)
+
+is_eid_assigned() {
+    busctl tree au.com.codeconstruct.MCTP1 | grep -q "/endpoints/$1"
+}
+
+setup_endpoint() {
+    local devname="$1"
+    local iface="$2"
+    local physaddr="$3"
+    local eid="$4"
+
+    if is_eid_assigned "$eid"; then
+        echo "Setting up $devname on $iface (EID=$eid, Addr=$physaddr): Skipped (EID already assigned)"
+        return
+    fi
+
+    if busctl call "au.com.codeconstruct.MCTP1" \
+        "/au/com/codeconstruct/mctp1/interfaces/$iface" \
+        "au.com.codeconstruct.MCTP.BusOwner1" AssignEndpointStatic ayy 1 "$physaddr" "$eid"; then
+        echo "Setting up $devname on $iface (EID=$eid, Addr=$physaddr): Success"
+    else
+        echo "Setting up $devname on $iface (EID=$eid, Addr=$physaddr): Failed" >&2
+    fi
+}
+
+if [[ -n "$dev" ]]; then
+    for entry in "${endpoint_map[@]}"; do
+        IFS=":" read -r devname iface physaddr eid <<< "$entry"
+        if [[ "$devname" == "$dev" ]]; then
+            setup_endpoint "$devname" "$iface" "$physaddr" "$eid"
+            exit 0
+        fi
+    done
+    echo "Device '$dev' not found in mapping" >&2
+    exit 1
+else
+    for entry in "${endpoint_map[@]}"; do
+        IFS=":" read -r devname iface physaddr eid <<< "$entry"
+        setup_endpoint "$devname" "$iface" "$physaddr" "$eid"
+    done
+fi
