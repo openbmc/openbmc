234192c860b54e868568f2bb05ee6bb9b9b716e6
Patrick Williams <patrick@stwcx.xyz>
2026-02-09 12:44:42 -0500

    eslint: switch to eslint/json
    openbmc-build-scripts needed to migrate to `@eslint/json` instead of
    `eslint-plugin-json`, due to incompatibility with eslint 10.x.  Copy
    the corresponding `eslint.config.js` from openbmc-build-scripts for
    compatibility.
    
    Signed-off-by: Patrick Williams <patrick@stwcx.xyz>
    Change-Id: I714f1b56a9a1e3411560cbbad150341b9a498063

--- eslint.config.js
+++ eslint.config.js
@@ -1,14 +1,13 @@
 const { defineConfig } = require("eslint/config");
-const jsonPlugin = require("eslint-plugin-json");
+const jsonPlugin = require("@eslint/json").default;
 
 module.exports = defineConfig([
     {
         files: ["**/*.json"],
         plugins: { json: jsonPlugin },
-        processor: "json/json",
-        rules: {
-            "json/*": ["error", "allowComments"],
-        },
+        // OpenBMC allows comments in JSON; treat as JSON5.
+        language: "json/json5",
+        extends: ["json/recommended"],
     },
     {
         ignores: ["**/meson-*/*.json", "subprojects/**/*.json"],
